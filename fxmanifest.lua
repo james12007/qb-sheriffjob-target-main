298d77cde2bd74f2c9f9fefd8ca21af2a4a33f02
JAMES_JB007 <meenakapil878j@gmail.com>
2022-05-14 10:05:34 +0530

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,46 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ᒎα爪ЄƧ#4411'
+description 'QB-Sheriffjob'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua',
+    '@qb-core/shared/locale.lua',
+    'locales/en.lua' -- Change this to your preferred language
+}
+
+client_scripts {
+	'@PolyZone/client.lua',
+	'@PolyZone/BoxZone.lua',
+	'@PolyZone/ComboZone.lua',
+	'client/main.lua',
+	'client/camera.lua',
+	'client/interactions.lua',
+	'client/job.lua',
+	'client/heli.lua',
+	--'client/anpr.lua',
+	'client/evidence.lua',
+	'client/objects.lua',
+	'client/tracker.lua'
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/vue.min.js',
+	'html/script.js',
+	'html/tablet-frame.png',
+	'html/fingerprint.png',
+	'html/main.css',
+	'html/vcr-ocd.ttf'
+}
+
+lua54 'yes'
